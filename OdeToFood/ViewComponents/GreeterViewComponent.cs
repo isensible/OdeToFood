778947791213f5d53e5aa47a47f57941a3b6ba6a
Edward James <ed.james@hotmail.co.uk>
2018-01-24 15:49:14 +0800

    ComponentViews - GreeterViewComponent added to _Layout

---
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private readonly IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            // Default is the default name of a view component
+            return View("Default", model);
+        }
+    }
+}
